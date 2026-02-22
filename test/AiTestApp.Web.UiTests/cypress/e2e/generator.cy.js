69deeecb9f13c2dd0b73117c96fbc42056332aca
Keith Jackson <temporal-net@live.co.uk>
2026-02-22 15:13:52 +0000

    Add comprehensive unit tests and Cypress UI tests for all new generators (TV Shows, Books, Albums, Dice)

--- test/AiTestApp.Web.UiTests/cypress/e2e/generator.cy.js
+++ test/AiTestApp.Web.UiTests/cypress/e2e/generator.cy.js
@@ -8,8 +8,14 @@ describe('Generator Pages', () => {
     cy.url().should('include', '/Generator/TvShow');
     cy.get('h1').should('be.visible');
     cy.get('p').contains('Genre:').should('be.visible');
-    cy.get('.btn').contains('Pick Another Random TV Show').click();
-    cy.url().should('include', '/Generator/TvShow');
+
+    cy.get('h1').then(($title1) => {
+        const firstTitle = $title1.text();
+        cy.get('.btn').contains('Pick Another Random TV Show').click();
+        cy.get('h1').should(($title2) => {
+            expect($title2.text()).to.not.equal(firstTitle);
+        });
+    });
   });
 
   it('navigates to Books page, selects a genre, and picks a random book', () => {
@@ -18,7 +24,17 @@ describe('Generator Pages', () => {
     cy.get('.btn').contains('Science Fiction').click();
     cy.url().should('include', '/Generator/Book?genre=Science%20Fiction');
     cy.get('h1').should('be.visible');
-    cy.get('.btn').contains('Pick Another Science Fiction Book').click();
+
+    cy.get('h1').then(($title1) => {
+        const firstTitle = $title1.text();
+        cy.get('.btn').contains('Pick Another Science Fiction Book').click();
+        cy.get('h1').should(($title2) => {
+            expect($title2.text()).to.not.equal(firstTitle);
+        });
+    });
+
+    cy.get('.btn').contains('Change Genre').click();
+    cy.url().should('include', '/Generator/BookGenre');
   });
 
   it('navigates to Albums page, selects a genre, and picks a random album', () => {
@@ -27,7 +43,25 @@ describe('Generator Pages', () => {
     cy.get('.btn').contains('Rock').click();
     cy.url().should('include', '/Generator/Album?genre=Rock');
     cy.get('h1').should('be.visible');
-    cy.get('.btn').contains(/Pick Another .* Album/).click();
+
+    cy.get('h1').then(($title1) => {
+        const firstTitle = $title1.text();
+        cy.get('.btn').contains(/Pick Another .* Album/).click();
+        cy.get('h1').should(($title2) => {
+            expect($title2.text()).to.not.equal(firstTitle);
+        });
+    });
+
+    cy.get('.btn').contains('Select Different Genre').click();
+    cy.url().should('include', '/Generator/AlbumPrompt');
+  });
+
+  it('picks a completely random album', () => {
+    cy.get('nav .nav-link').contains('Albums').click();
+    cy.url().should('include', '/Generator/AlbumPrompt');
+    cy.get('.btn').contains('Pick One at Random').click();
+    cy.url().should('include', '/Generator/Album');
+    cy.get('h1').should('be.visible');
   });
 
   it('navigates to Dice page, selects a die, and rolls it', () => {
@@ -40,5 +74,7 @@ describe('Generator Pages', () => {
         expect(val).to.be.at.least(1).and.at.most(20);
     });
     cy.get('.btn').contains('Roll Again').click();
+    cy.get('.btn').contains('Select Different Die').click();
+    cy.url().should('include', '/Generator/Dice');
   });
 });
